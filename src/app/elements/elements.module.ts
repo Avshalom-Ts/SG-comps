bcb4caee45d0f01b6bd8557b66e43d08d99c9343
Azdevpp <azdevpp@gmail.com>
2022-10-22 02:14:30 +0300

    ComponentDivider

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceHolderComponent } from './place-holder/place-holder.component';
@@ -8,7 +9,7 @@ import { TimesDirective } from './times.directive';
 
 @NgModule({
   declarations: [ElementsHomeComponent, PlaceHolderComponent, TimesDirective],
-  imports: [CommonModule, ElementsRoutingModule],
+  imports: [CommonModule, ElementsRoutingModule, SharedModule],
   exports: [],
 })
 export class ElementsModule {}
