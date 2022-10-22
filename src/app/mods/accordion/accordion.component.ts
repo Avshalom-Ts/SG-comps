ba9e98009ed6b690a73a70c7188d7d5d0f8e9df0
Azdevpp <azdevpp@gmail.com>
2022-10-23 01:59:21 +0300

    AccordionComponentFinish

--- src/app/mods/accordion/accordion.component.ts
+++ src/app/mods/accordion/accordion.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
-  styleUrls: ['./accordion.component.css']
+  styleUrls: ['./accordion.component.css'],
 })
 export class AccordionComponent implements OnInit {
+  @Input() items: any[] = [];
+  openedItemIndex: number = NaN;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClick(index: number) {
+    if (index === this.openedItemIndex) {
+      this.openedItemIndex = NaN;
+    } else {
+      this.openedItemIndex = index;
+    }
   }
-
 }
