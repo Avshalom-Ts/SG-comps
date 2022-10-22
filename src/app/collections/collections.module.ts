909c4946d0edff497747c3be796a9024e09edc6e
Azdevpp <azdevpp@gmail.com>
2022-10-22 21:42:05 +0300

    TabsComponentFinish

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -8,9 +8,10 @@ import { SharedModule } from '../shared/shared.module';
 import { BiographyComponent } from './biography/biography.component';
 import { CompaniesComponent } from './companies/companies.component';
 import { PatrnersComponent } from './patrners/patrners.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
-  declarations: [CollectionHomeComponent, TableComponent, BiographyComponent, CompaniesComponent, PatrnersComponent],
+  declarations: [CollectionHomeComponent, TableComponent, BiographyComponent, CompaniesComponent, PatrnersComponent, TabsComponent],
   imports: [CommonModule, CollectionsRoutingModule, SharedModule],
   exports: [],
 })
