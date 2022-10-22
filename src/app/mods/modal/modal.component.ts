aa37aa77db27ca68137be2e47cb6100c7d8e961c
Azdevpp <azdevpp@gmail.com>
2022-10-23 00:33:59 +0300

    ModalComponentFix

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private el: ElementRef) {
+    // console.log(this.el.nativeElement);
   }
 
+  ngOnInit(): void {
+    //Help to Append the Modal window to the body
+    document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy() {
+    //Remove the Append Modal window from the body
+    this.el.nativeElement.remove();
+  }
 }
